9465ad711ad0cfdd126c20eb3236f811ab1aed12
Luis Sagnay <sagnayluis99@gmail.com>
2026-03-15 21:54:15 -0500

    Refactor code structure for improved readability and maintainability

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,43 +1,33 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/pages/home/home';
-import { TextCaseTool } from './components/pages/text-case-tool/text-case-tool'; 
+import { TextCaseTool } from './components/pages/text-case-tool/text-case-tool';
 import { Base64Tool } from './components/pages/base64-tool/base64-tool';
 import { WordCount } from './components/pages/word-count/word-count';
-import { HashGenerator } from './components/pages/hash-generator/hash-generator';
 import { TextJsonConverter } from './components/pages/text-json-converter/text-json-converter';
-import { ReversePalindromeTool } from './components/pages/reverse-palindrome-tool/reverse-palindrome-tool';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: Home,
-    },
-    {
-        path: 'text-case',
-        component: TextCaseTool,
-    },
-    {
-        path: 'base64',
-        component: Base64Tool,
-    },
-    {
-        path: 'word-count',
-        component: WordCount,
-    },
-    {
-        path: 'hash-generator',
-        component: HashGenerator,
-    },
-    {
-        path: 'text-json-converter',
-        component: TextJsonConverter,
-    },
-    {
-        path: 'reverse-palindrome',
-        component : ReversePalindromeTool,
-    },
-    {
-        path: '**',
-        redirectTo: '/',
-    }
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'text-case',
+    component: TextCaseTool,
+  },
+  {
+    path: 'base64',
+    component: Base64Tool,
+  },
+  {
+    path: 'word-count',
+    component: WordCount,
+  },
+  {
+    path: 'text-json-converter',
+    component: TextJsonConverter,
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+  },
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ export default {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
