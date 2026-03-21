d137028348e2b57f9f828794888061d2b9e748f4
Luis Sagnay <sagnayluis99@gmail.com>
2026-03-20 21:47:04 -0500

    Update allowed origins and add Vercel configuration file

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,9 @@ SUPPORTED_FORMATS: dict[str, str] = {
 }
  
 ALLOWED_ORIGINS = [
-    "http://localhost:4200",
     "http://127.0.0.1:4200",
+    "https://octool.vercel.app/",
+    "https://octool-efw3rvzi4-luis-sagxs-projects.vercel.app/",
+    "https://octool-git-main-luis-sagxs-projects.vercel.app/",
 ]
  
